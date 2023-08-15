401852aad9e516db9c60e182adb0f23822496f02
Gary Griffaw <ggriffaw@yahoo.com>
2023-08-15 14:06:50 -0500

    Set ingredients to empty array if there are no ingredients

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -24,6 +25,16 @@ export class DataStorageService {
     this.http
       .get<Recipe[]>(
         'https://ng-course-recipe-book-dd0a4-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
       )
       .subscribe((recipes) => {
         this.recipeService.setRecipes(recipes);

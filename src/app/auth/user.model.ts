4ec8a10f457f3aee200c64c9fc4bbe5cdcef9473
Gary Griffaw <ggriffaw@yahoo.com>
2023-08-19 17:18:07 -0500

    Add auto logout functionality

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -3,11 +3,11 @@ export class User {
     public email: string,
     public id: string,
     private _token: string,
-    private _tokenExpiationDate: Date
+    private _tokenExpirationDate: Date
   ) {}
 
   get token() {
-    if (!this._tokenExpiationDate || new Date() > this._tokenExpiationDate) {
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
       return null;
     }
     return this._token;
